acb27ae77bed6ca3ed9b50ca16bf262f97f37f32
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-10-25 23:37:50 -0600

    adding option

--- Quiz-game.py
+++ Quiz-game.py
@@ -1,3 +1,12 @@
 print("Welcome to my computer quiz!")
 
+playing = input("Do you want to play? ")
 
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print('Correct!')
