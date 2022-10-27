323340a6d6aaf8478bf43227436ff6e57713da2f
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-10-26 20:38:37 -0600

    improving game

--- Quiz-game.py
+++ Quiz-game.py
@@ -13,26 +13,21 @@ if answer == "central processing unit":
 else:
     print("Incorrect!")
 
-answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
     print('Correct!')
 else:
     print("Incorrect!")
 
-answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
     print('Correct!')
 else:
     print("Incorrect!")
 
-answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+answer = input("What does PSU stand for? ")
+if answer == "power supply":
     print('Correct!')
 else:
     print("Incorrect!")
 
-answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
-    print('Correct!')
-else:
-    print("Incorrect!")
