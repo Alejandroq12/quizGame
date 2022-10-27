7f2d74f1241b4fcc698ebd91eb7ad2e2624e55a6
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-10-26 20:47:25 -0600

    adding .lower

--- Quiz-game.py
+++ Quiz-game.py
@@ -2,32 +2,31 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play :)")
 
 answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print('Correct!')
 else:
     print("Incorrect!")
 
 answer = input("What does GPU stand for? ")
-if answer == "graphics processing unit":
+if answer.lower() == "graphics processing unit":
     print('Correct!')
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for? ")
-if answer == "random access memory":
+if answer.lower() == "random access memory":
     print('Correct!')
 else:
     print("Incorrect!")
 
 answer = input("What does PSU stand for? ")
-if answer == "power supply":
+if answer.lower() == "power supply":
     print('Correct!')
 else:
     print("Incorrect!")
-
